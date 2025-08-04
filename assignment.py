ddfd400a6b74594a4cf26f81e3334b55f9efd07d
Nakhaima254 <nakhaimaisaac068gmail.com>
2025-08-04 11:58:49 +0300

    Add commented assignment.py

---
+++ assignment.py
@@ -0,0 +1,10 @@
+# Prompt the user to enter the first number and convert it to a float
+num1 = float(input("Enter the first number: "))
+# Prompt the user to enter the second number and convert it to a float
+num2 = float(input("Enter the second number: "))
+# Calculate the sum of the two numbers
+sum_result = num1 + num2
+
+# Print the results
+print(f"Results of your two numbers:")
+print(f"Sum: {sum_result}")  # ➕
